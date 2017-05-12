cd26a131ef3dac4c2ae3ff2da6848336a57a9e40
markslingerland <markslingerland@hotmail.com>
2017-05-12 23:46:54 +0200

    Made the side bar smaller

--- ClientApp/components/NavMenu.tsx
+++ ClientApp/components/NavMenu.tsx
@@ -12,7 +12,7 @@ export class NavMenu extends React.Component<any, void> {
                         <span className='icon-bar'></span>
                         <span className='icon-bar'></span>
                     </button>
-                    <Link className='navbar-brand' to={ '/' }>WebApplicationBasic</Link>
+                    <Link className='navbar-brand' to={ '/' }>Portfolio</Link>
                 </div>
                 <div className='clearfix'></div>
                 <div className='navbar-collapse collapse'>
